6477df8d388d94109835799e18462a4f09524f67
Devprasath17 <devprasathkandavel@gmail.com>
2026-05-19 19:53:44 +0530

    Updated portfolio UI and animations

--- src/data/projects.js
+++ src/data/projects.js
@@ -15,25 +15,27 @@ const projects = [
   },
   {
     id: 'shadowcommerce',
-    title: 'ShadowCommerce Engine',
-    stack: 'Next.js, Redis, Tailwind',
+    title: 'E commerce',
+    stack: 'React.js, Node.js, MongoDB',
     description: [
       'Optimized API response time to < 200ms using Redis caching.',
       'Server-side rendering for improved SEO and initial load speed.',
     ],
-    icon: 'shopping_bag',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuCNceAax7auBC5JxNkzqhNcO3y3VTHa-xinXvypVnlIiLdAE92YhC7grhnK5112rF5cErjHRncCJGXpKglBsL5DHVYmXlmDvaXxzX1B3rwQJsUUNkIE18-UoFlelUusEsC1w5jUfWOnN8BEHbHLN-OSlI0no-W6t3TIAq8XUmdEOEtW6KwmYwMk_q4Gz2FnculhTnfVAy6Ub4Cllk3Jc-pKsigK4ZN1xwzuVEGhkQ0iNSqK6TsFlZO__0zMarXv5mc6euCge_jP2n1q',
     liveUrl: '#',
     sourceUrl: '#',
   },
   {
     id: 'nexus-event-link',
-    title: 'Nexus Event Link',
-    stack: 'React Native, Firebase',
+    title: 'Event Management App',
+    stack: 'React Native, Node.js, AWS Cloud',
     description: [
       'Real-time sync using Firebase Cloud Firestore for schedules.',
       'Push notifications for attendee engagement and updates.',
     ],
-    icon: 'event',
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuCyNF6yMrv_z-IaCUPv_zUhbU0g2aRWiCXd1bGWlx7P_rU3wYxYjb_6Gubunc8HdexS5LdXQnzK4h3CRMug3kyxlLpXaffgm60t66FNnkOEPsRsgtZjKeD20Qz-5EuQYxMQZf5zgisAZL4MAlj6b4sKhFgnuGp_hhrNNNQC23_p9kaBoeHrwwj2WT9jT91ja_IjLRcyF0JQifFvsReQEGvksB2nbQOthbTmNPGDdd2ELgTbFmKqV03p6i-wS_oEne-8c9hXXg5zfjuN',
     liveUrl: '#',
     sourceUrl: '#',
   },
